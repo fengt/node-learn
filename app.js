d854c5e96087194c4b30126cd12b4668292a1151
fhalcyon <fhalcyon@gmail.com>
2018-05-19 22:35:22 +0800

    simple crawler with superagent & cheerio

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require('express');
-var utility = require('utility');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  var q = req.query.q;
+app.get('/', function (req, res, next) {
+  
+  superagent.get('https://cnodejs.org')
+    .end(function (err, sres) {
+    	if (err) {
+    		return next(err);
+    	}
 
-  var md5Value = utility.md5(q);
-  res.send(md5Value);
+    	var $ = cheerio.load(sres.text);
+    	var items = [];
+    	$('#topic_list .topic_title').each(function (idx, element) {
+    		var $element = $(element);
+    		items.push({
+    			title: $element.attr('title'),
+    			href: $element.attr('href')
+    		});
+    	});
+
+    	res.send(items);
+    });
 });
 
 app.listen(3000, function () {
